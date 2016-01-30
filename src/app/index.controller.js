45138c75f3723abe271417b5262989617ae37375
Uzi <sksouza.art@gmail.com>
2016-01-30 14:10:29 -0500

    More minor tweaks.

--- src/app/index.controller.js
+++ src/app/index.controller.js
@@ -9,5 +9,8 @@
   function indexController($scope, $state) {
     $scope.isOpen = false;
     $scope.state = $state;
+    $scope.isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+    //console.log($scope.isSafari);
+    //found here: http://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
   }
 })();
